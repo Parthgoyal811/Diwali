0207a73b386fbba720f6e370d5cabc61e3920244
Coding__X <144516028+Parthgoyal811@users.noreply.github.com>
2023-11-11 17:54:18 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+function showFireworks() {
+    const fireworksContainer = document.getElementById("fireworks");
+
+    // You can add more complex fireworks animations here
+    fireworksContainer.innerHTML = "<img src='image/6.gif' alt='Fireworks'>";
+}
